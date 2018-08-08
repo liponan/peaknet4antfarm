abbf4393885b1f76f9b63d7a726bf9dcf548e298
leeneil <lishantreasure@gmail.com>
2018-08-08 10:11:45 -0700

    return model instead of numpy

---
+++ peaknet.py
@@ -0,0 +1,39 @@
+import os
+import os.path as osp
+import time
+from torch.autograd import Variable
+import torch as t
+import sys
+sys.path.append(os.path.abspath('../pytorch-yolo-v3'))
+from darknet import Darknet
+from preprocess import prep_image, inp_to_image
+from util import loss, loadLabels, IOU
+
+
+class Peaknet():
+
+    '''
+    “imgs: variable name”
+    “labels: variable name"
+    '''
+    def train( self, model, imgs, labels, box_size = 7, tmp_path="tmps" ):
+        model = None
+        return model
+
+    '''
+    “imgs: string”
+    “labels: string"
+    '''
+    def train_from_shm( self, model, imgs, labels, box_size = 7, tmp_path="tmps" ):
+        model = None
+        return model
+
+
+    def test( self, imgs, box_size = 7):
+        results = None
+        return results
+
+
+    def validate( self, imgs, golden_labels, box_size = 7, tmp_path="tmps" ):
+        results = None
+        return results
