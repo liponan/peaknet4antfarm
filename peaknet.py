ad9489e44b20d56f7c588135665cf15cda8ac1ba
leeneil <lishantreasure@gmail.com>
2018-08-17 10:59:38 -0700

    for testing

--- peaknet.py
+++ peaknet.py
@@ -1,41 +1,54 @@
 import os
 import os.path as osp
 import time
-from torch.autograd import Variable
+# from torch.autograd import Variable
 import torch as t
 import sys
-sys.path.append(os.path.abspath('../pytorch-yolo-v3'))
+sys.path.append(os.path.abspath('../pytorch-yolo2'))
 from darknet import Darknet
-from preprocess import prep_image, inp_to_image
-from util import loss, loadLabels, IOU
+# from train import train_peaknet
+# from preprocess import prep_image, inp_to_image
+# from util import loss, loadLabels, IOU
 
+# workPath = "/reg/neh/home/liponan/ai/peaknet4antfarm/"
+workPath = "../pytorch-yolo2/"
 
 class Peaknet():
+
+    def __init__(self):
+        self.model = None
+
+
+    def loadDNWeights( self, cfgPath, weightsPath ):
+        self.model = Darknet(workPath + 'cfg/newpeaksv5.cfg')
+        self.model.load_weights(workPath + "weights/newpeaksv5.backup")
 
     '''
     "imgs: variable name"
     "labels: variable name"
     '''
-    def train( self, model, imgs, labels, box_size = 7, tmp_path="tmps" ):
-        dn = Darknet('cfg/newpeaksv9-yolo.cfg')
-        dn.load_weights("weights/newpeaksv9_40000.weights")
-        return dn
+
+    def train( self, imgs, labels, box_size = 7 ):
+        print("training...")
+
+    def model( self ):
+        return self.model
 
     '''
     "imgs: string"
     "labels: string"
     '''
-    def train_from_shm( self, model, imgs, labels, box_size = 7, tmp_path="tmps" ):
+    def train_from_shm( self, imgs, labels, box_size = 7 ):
         model = None
         return model
 
 
-    def test( self, imgs, box_size = 7):
+    def test( self, imgs, box_size = 7 ):
         results = None
         return results
 
 
-    def validate( self, imgs, golden_labels, box_size = 7, tmp_path="tmps" ):
+    def validate( self, imgs, golden_labels, box_size = 7 ):
         results = None
         return results
 
